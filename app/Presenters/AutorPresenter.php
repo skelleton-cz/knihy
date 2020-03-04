83c124a240b7d4a25177c825d3fab1cab0479245
Ondrej Lang <ondrej.lang@dain.cz>
2020-03-04 14:22:29 +0100

    Vytvoreni formulářu pro přidávání nových autorů a knih

--- app/Presenters/AutorPresenter.php
+++ app/Presenters/AutorPresenter.php
@@ -4,6 +4,9 @@
 namespace App\Presenters;
 
 
+use Nette\Application\UI\Form;
+use Nette\Utils\ArrayHash;
+
 class AutorPresenter extends BasePresenter
 {
 
@@ -11,5 +14,27 @@ class AutorPresenter extends BasePresenter
     {
         $this->template->autori = $this->autoriModel->getTableAutori();
         $this->template->knihy = $this->knihyModel->getTableKnihy();
+    }
+
+    protected function createComponentAutorForm()
+    {
+        $form = new Form();
+        $form->addHidden('id');
+        $form->addText('jmeno', 'Jméno autora: ')
+            ->setRequired();
+        $form->addText('prijmeni', 'Příjmení autora: ')
+            ->setRequired();
+        $form->addInteger('rokNarozeni', 'Rok narození: ');
+        $form->addSubmit('save', "Uložit");
+
+        $form->onSuccess[] = [$this, 'autorFormUlozit'];
+
+        return $form;
+    }
+
+    public function autorFormUlozit(Form $form, ArrayHash $vals)
+    {
+        dump($vals);
+        exit();
     }
 }
--- app/Presenters/KnihaPresenter.php
+++ app/Presenters/KnihaPresenter.php
@@ -4,10 +4,47 @@
 namespace App\Presenters;
 
 
+use Nette\Application\UI\Form;
+use Nette\Utils\ArrayHash;
+
 class KnihaPresenter extends BasePresenter
 {
+    protected $autori;
+
     public function renderDefault()
     {
         $this->template->knihy = $this->knihyModel->getSeznamKnih();
     }
+
+    /**
+     * Metoda `actionCokoliv` se vzdy provede pred metodou `renderCokoliv`
+     * Behem vykreslovani formulare uzivateli je jiz potreba mit vyplnena data (napr obsah ciselniku OPTIONS)
+     * v opacnem pripade by potvrzeni formulare vyvolalo bezpecnostni chybu
+     */
+    public function actionAdd()
+    {
+        $this->autori = $this->autoriModel->getSeznamAutoru()->fetchPairs('id', 'prijmeni');
+    }
+
+    protected function createComponentKnihaForm()
+    {
+        $form = new Form();
+        $form->addHidden('id');
+        $form->addText('nazev', 'Nazev knihy: ')
+            ->setRequired();
+        $form->addSelect('autor_id', 'Autor: ', $this->autori);
+        $form->addInteger('rokVydani', 'Rok vydání: ');
+        $form->addSubmit('save', "Uložit");
+
+        $form->onSuccess[] = [$this, 'knihaFormUlozit'];
+
+        return $form;
+    }
+
+    public function knihaFormUlozit(Form $form, ArrayHash $vals)
+    {
+        dump($vals);
+        exit();
+    }
+
 }
